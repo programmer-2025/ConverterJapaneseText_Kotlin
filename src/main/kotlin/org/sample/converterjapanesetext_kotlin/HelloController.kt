ca4a71c4c523b805dd18744a173a9df4abe91e8d
programmer-2025 <snowsansub011307@gmail.com>
2026-08-02 12:57:08 +0900

    ＜追加＞　UIを仮追加しました

--- src/main/kotlin/org/sample/converterjapanesetext_kotlin/HelloController.kt
+++ src/main/kotlin/org/sample/converterjapanesetext_kotlin/HelloController.kt
@@ -2,13 +2,29 @@ package org.sample.converterjapanesetext_kotlin
 
 import javafx.fxml.FXML
 import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import java.net.URL
 
 class HelloController {
+
+    lateinit var beforeConvertField: TextField
+    lateinit var afterConvertField: TextField
+
     @FXML
     private lateinit var welcomeText: Label
 
     @FXML
-    private fun onHelloButtonClick() {
-        welcomeText.text = "Welcome to JavaFX Application!"
+    private fun onClick() {
+        if (beforeConvertField.text.isEmpty()) {
+
+        }
+        else {
+
+        }
+    }
+
+    private fun convertText(text: String): String? {
+        val url = URL("");
+        return null;
     }
 }
